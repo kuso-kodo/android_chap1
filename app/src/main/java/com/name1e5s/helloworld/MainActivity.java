57124a9cf3255fa5649c4eebc9896ef2ed81e5e9
name1e5s <name1e5s@qq.com>
2020-03-22 00:54:59 +0800

    Add screenshot and log

--- app/src/main/java/com/name1e5s/helloworld/MainActivity.java
+++ app/src/main/java/com/name1e5s/helloworld/MainActivity.java
@@ -18,11 +18,12 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     final String msg = "Hello World!";
+    final String TAG = "name1e5s";
     private int tapCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("name1e5s", msg);
+        Log.d(TAG, msg);
         setContentView(R.layout.activity_main);
 
         final Button buttonTapHere = findViewById(R.id.buttonTapHere);
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 tapCount += 1;
                 textViewTapHere.setText(String.format("You tapped th button %d times", tapCount));
+                Log.d(TAG, String.format("You tapped th button %d times", tapCount));
             }
         });
 
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 double value = progress * 100.0f / seekBar.getMax();
                 textViewSeekBar.setText(String.format("%.2f%%", value));
+                Log.d(TAG, String.format("Current progress: %.2f%%", value));
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) { }
